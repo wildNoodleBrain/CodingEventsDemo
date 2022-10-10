997b240554e4bb65ef66db5d0d56193886a18dbe
wildNoodleBrain <104745283+wildNoodleBrain@users.noreply.github.com>
2022-10-09 20:36:04 -0500

    Created Model class and Model Property

--- CodingEventsDemo/Controllers/EventsController.cs
+++ CodingEventsDemo/Controllers/EventsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodingEventsDemo.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,7 +12,7 @@ namespace coding_events_practice.Controllers
     public class EventsController : Controller
     {
 
-        static private Dictionary<string, string> Events = new Dictionary<string, string>();
+        static private List<Event> Events = new List<Event>();
 
         // GET: /<controller>/
         public IActionResult Index()
@@ -26,11 +27,11 @@ namespace coding_events_practice.Controllers
             return View();
         }
 
-        [HttpPost]
+        [HttpPost] 
         [Route("Events/Add")]
-        public IActionResult NewEvent(string name, string desc = "")
+        public IActionResult NewEvent(string name, string desc)
         {
-            Events.Add(name, desc);
+            Events.Add(new Event(name, desc));
             
 
             return Redirect("/Events");
---
+++ CodingEventsDemo/Models/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace CodingEventsDemo.Models
+{
+    public class Event
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+     
+        public Event(string name, string description)
+        {
+            Name = name;
+            Description = description;  
+        }
+
+        public override string ToString()
+        {
+            return Name;
+
+        }
+    }
+}
