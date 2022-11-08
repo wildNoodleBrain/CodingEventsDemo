b999341559e0a4a0c05147bfbb2d8b342f5a6073
wildNoodleBrain <104745283+wildNoodleBrain@users.noreply.github.com>
2022-11-07 19:59:04 -0600

    Data Stores

---
+++ CodingEventsDemo/Data/EventDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CodingEventsDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodingEventsDemo.Data
+{
+    public class EventDbContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+        public EventDbContext(DbContextOptions<EventDbContext> options) : base(options)
+        {
+
+        }
+    }
+  }
