878e0f492527834f13ac463c73a4f9b92b5bd00a
wildNoodleBrain <104745283+wildNoodleBrain@users.noreply.github.com>
2022-11-13 16:39:15 -0600

    17.4.3. Adding a New Table to the Database  I keep getting an error More than one DbContext was found

--- CodingEventsDemo/Data/EventDbContext.cs
+++ CodingEventsDemo/Data/EventDbContext.cs
@@ -7,11 +7,13 @@ namespace CodingEventsDemo.Data
     public class EventDbContext : DbContext
     {
         public DbSet<Event> Events { get; set; }
-        public DbSet<EventCategory> Categories { get; set; }
 
+         public DbSet<EventCategory> Categories { get; set; }
         public EventDbContext(DbContextOptions<EventDbContext> options) : base(options)
         {
 
         }
+            
+
     }
 }
--- CodingEventsDemo/Models/EventCategory.cs
+++ CodingEventsDemo/Models/EventCategory.cs
@@ -1,35 +1,33 @@
 ﻿using CodingEventsDemo.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
 
 namespace CodingEventsDemo.Models
 {
-    public class EventCategory
+    public class EventCategory : DbContext
     {
         public string Name { get; set; }
+
         public int Id { get; set; }
+        public string Categories { get; set; }
+
         public EventCategory(string name)
         {
             Name = name;
         }
+
         public EventCategory()
         {
-            
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(base.GetHashCode(), Database, ChangeTracker, Model, ContextId, Name, Id);
         }
     }
 }
-//@using CodingEventsDemo.Models;
-//using Microsoft.EntityFrameworkCore;
-
-//namespace CodingEventsDemo.Data
-//{
-  //  public class EventDbContext : DbContext
-    //{
-      //  public DbSet<Event> Events { get; set; }
-
-        //public EventDbContext(DbContextOptions<EventDbContext> options)
-          //    : base(options)
-        //{
-        //}
-   // }
-//}
