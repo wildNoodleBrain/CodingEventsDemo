be637f374269ae594091d411661afc50fef2ceb4
wildNoodleBrain <104745283+wildNoodleBrain@users.noreply.github.com>
2022-11-20 19:49:19 -0600

    18.5.6. Displaying Tags in the Detail View

---
+++ CodingEventsDemo/Controllers/TagController.cs
@@ -0,0 +1,80 @@
+﻿using CodingEventsDemo.Data;
+using CodingEventsDemo.Models;
+using CodingEventsDemo.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodingEventsDemo.Controllers
+{
+    public class TagController : Controller
+    {
+        private EventDbContext context;
+
+        public TagController(EventDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<Tag> tags = context.Tags.ToList();
+            return View(tags);
+        }
+
+        public IActionResult Add()
+        {
+            Tag tag = new Tag();
+            return View(tag);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Tag tag)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Tags.Add(tag);
+                context.SaveChanges();
+                return Redirect("/Tag/");
+            }
+
+            return View("Add", tag);
+        }
+        public IActionResult AddEvent(int Id)
+        {
+            Event theEvent = context.Events.Find(Id);
+            List<Tag> possibleTags = context.Tags.ToList();
+
+            AddEventTagViewModel viewModel = new AddEventTagViewModel(theEvent, possibleTags);
+
+            return View(viewModel);
+
+        }
+
+        [HttpPost]
+        public IActionResult AddEvent(AddEventTagViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                int eventId = viewModel.EventId;
+                int tagId = viewModel.TagId;
+
+                EventTag eventTag = new EventTag
+                {
+                    EventId = eventId,
+                    TagId = tagId,
+                };
+
+                context.EventTags.Add(eventTag);
+                context.SaveChanges();
+
+                return Redirect("/Events/Detail/" + eventId);
+            }
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ CodingEventsDemo/Models/EventTag.cs
@@ -0,0 +1,14 @@
+﻿namespace CodingEventsDemo.Models
+{
+    public class EventTag
+    {
+        public int EventId { get; set; }
+        public Event Event { get; set; }
+        public int TagId { get; set; }
+        public Tag Tag { get; set; }
+        public EventTag()
+        {
+
+        }
+    }
+}
